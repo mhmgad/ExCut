6bdf4afa38e4531307db3b5d71b1d6b26851b666
Mohamed Gad-Elrab <mhmgad@gmail.com>
2021-07-07 17:49:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from ampligraph import __version__ as version
+
+setup_params = dict(name='ampligraph',
+                    version=version,
+                    description='ExCut: A Python library for Explainable Entity Clustering',
+                    url='https://github.com/mhmgad/ExCut',
+                    author='Mohamed Gad-Elrab',
+                    author_email='gadelrab@mpi-inf.mpg.de',
+                    license='Apache 2.0',
+                    packages=find_packages(exclude=('tests', 'docs')),
+                    include_package_data=True,
+                    zip_safe=False,
+                    install_requires=[
+                      
+                    ])
